c1c51df7bdb1a0e176c3dbec5e5d6b6f8e70960a
junhan <junhanj@hotmail.com>
2021-08-27 07:00:07 +0000

    added timestamps to pub msg

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,6 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export PKG_CONFIG_PATH="/home/ubuntu/catkin_ws3/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/ubuntu/catkin_ws3/build'
-export PYTHONPATH="/home/ubuntu/catkin_ws3/devel/lib/python3/dist-packages:$PYTHONPATH"
--- src/hps_camera/src/ros_camera_client.cpp
+++ src/hps_camera/src/ros_camera_client.cpp
@@ -59,6 +59,7 @@ void *User_Func(HPS3D_HandleTypeDef *handle, AsyncIObserver_t *event)
 					//Convert the cloud to ROS message
 					pcl::toROSMsg(cloud, output);
 					output.header.frame_id = "laser";
+					output.header.stamp = ros::Time::now();
 					//camera_pub.publish(output);	
 					//msg.distance_average = event->MeasureData.full_depth_data->distance_average;
 					//camera_pub.publish(msg);
